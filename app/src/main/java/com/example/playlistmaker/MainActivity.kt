be50d4509069d05b2d984253ff24361a2a8113ca
VankaGam <lumiamicrosoft@yandex.ru>
2024-11-22 22:18:19 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val displayButton1 = findViewById<LinearLayout>(R.id.setting)
+        val displayButton2 = findViewById<LinearLayout>(R.id.poisk)
+        val displayButton3 = findViewById<LinearLayout>(R.id.media)
+
+        displayButton1.setOnClickListener {
+            val displayIntent1 = Intent(this, SettingActivity::class.java)
+            startActivity(displayIntent1)
         }
+        displayButton2.setOnClickListener {
+            val displayIntent2 = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent2)
+        }
+        displayButton3.setOnClickListener {
+            val displayIntent3 = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(displayIntent3)
+        }
+
     }
 }
